98d73e6cfe987bb3e14db963a3ea28148bc94c21
BigDoggy87 <48752972+Tarseus@users.noreply.github.com>
2024-09-08 11:25:19 +0800

    add compare norm_data vs. data

--- pre_process_1d.py
+++ pre_process_1d.py
@@ -7,6 +7,9 @@ import os
 from matplotlib.ticker import ScalarFormatter
 import pandas as pd
 from utils import *
+import imageio
+import shutil
+from tqdm import tqdm
 
 data_folder = './data'
 auxiliary_folder = './auxiliary'
@@ -32,6 +35,47 @@ def set_normalization_output_dir(output_dir='./normalized_data'):
     else:
         print(f'Output directory {output_dir} already exists')
     
+    
+def save_video(dataset, train_wc, tmp_dir='./tmp'):
+    bounds = np.loadtxt(f"{auxiliary_folder}/breakpoints.csv", delimiter=',', skiprows=1, usecols=(1, 2, 3, 4)).astype(np.int16)
+    os.makedirs(tmp_dir, exist_ok=True)
+    plt.figure()
+    file_names = []
+    for i in tqdm(range(train_wc.shape[0])):
+    # for i in tqdm(range(10)):
+        data, norm_data, bound = dataset[i], train_wc[i], bounds[i]
+        # data = (data - np.min(data)) / (np.max(data) - np.min(data))
+        data1 = data / np.max(data)
+        fig, ax1 = plt.subplots(figsize=(12, 6))
+        ax2 = ax1.twinx()
+        ax1.plot(data1, color='#4E79A7', alpha=0.7, label="Data")
+        ax2.plot(norm_data, color='#F28E2B', label="Normalized Data")
+        # ax1.set_title('Light-curves from the train set') 
+        ax1.legend(loc='upper left')
+        ax2.legend(loc='upper right')
+        
+        for j in range(2):
+            ax1.axvline(x=bound[2*j + 0], color="r", linestyle="--")
+            ax1.axvline(x=bound[2*j + 1], color="r", linestyle="--")
+            ax1.axvspan(bound[2*j + 0], bound[2*j + 1], color="gray", alpha=0.3)
+            ax2.axvline(x=bound[2*j + 0], color="r", linestyle="--")
+            ax2.axvline(x=bound[2*j + 1], color="r", linestyle="--")
+            ax2.axvspan(bound[2*j + 0], bound[2*j + 1], color="gray", alpha=0.3)
+        
+        ax1.set_xlabel('Time')
+        ax1.set_ylabel('Data')
+        ax2.set_ylabel('Normalized Data')
+        
+        fig.tight_layout()
+        fig.suptitle('Light-curve from the train set')
+        plt.savefig(f'tmp/{i}.png')
+        plt.close()
+        file_names.append(f'tmp/{i}.png')
+    with imageio.get_writer('norm_light_curve.mp4', mode='I', fps=1.25) as writer:
+        for filename in file_names:
+            image = imageio.imread(filename)
+            writer.append_data(image)
+    shutil.rmtree('tmp')
 def pre_process_1d():
     train_solution = np.loadtxt(f'{auxiliary_folder}/train_labels.csv', delimiter = ',', skiprows = 1)
     targets = train_solution[:,1:]
@@ -42,10 +86,12 @@ def pre_process_1d():
     dataset = np.concatenate([signal_AIRS_diff_transposed_binned, FGS_column[:,:, np.newaxis,:]], axis = 2)
     # shape: 673, 187, 283, 32; num_samples, num_time_step, num_wavelength, num_spatial_dim
     dataset = dataset.sum(axis=3)
+    dataset_2d = dataset.sum(axis=2)
     # shape: 673, 187, 283; num_samples, num_time_step, num_wavelength
     dataset_norm = norm_star_spectrum(dataset)
     dataset_norm = np.transpose(dataset_norm,(0,2,1))
-    N_train = 8*N//10
+    # N_train = 8*N//10
+    N_train = N
     train_obs, valid_obs, list_index_train = split(dataset_norm, N_train)
     train_targets, valid_targets = targets[list_index_train], targets[~list_index_train]
     signal_AIRS_diff_transposed_binned = signal_AIRS_diff_transposed_binned.sum(axis=3)
@@ -61,13 +107,7 @@ def pre_process_1d():
     # Normalize the targets 
     train_targets_wc_norm, valid_targets_wc_norm, min_train_valid_wc, max_train_valid_wc = normalize(train_targets_wc, valid_targets_wc)
     
-    plt.figure()
-    for i in range (200) : 
-        plt.plot(train_wc[-i], '-', alpha = 0.5)
-    plt.title('Light-curves from the train set') 
-    plt.xlabel('Time')
-    plt.ylabel('Normalized flux')
-    plt.show()
+    save_video(dataset_2d, train_wc)
     return train_wc, valid_wc, train_targets_wc_norm, valid_targets_wc_norm, min_train_valid_wc, max_train_valid_wc
 
 if __name__ == '__main__':
